0fb9ada42e8760a20a09609389c55e1603f9b182
George <180932462+George-RD@users.noreply.github.com>
2026-07-29 16:34:59 +0400

    test(python): keep version regression compatible with Python 3.8

--- python/tests/test_version_provenance.py
+++ python/tests/test_version_provenance.py
@@ -1,8 +1,5 @@
 import importlib.metadata
-import os
-import sys
 import unittest
-from unittest import mock
 
 import mag_memory
 
@@ -20,26 +17,8 @@ class VersionProvenanceTests(unittest.TestCase):
             mag_memory._binary_version(),
         )
 
-    @unittest.skipIf(os.name == "nt", "Unix exec path is tested on Linux CI")
-    def test_main_downloads_the_distribution_version(self):
-        with (
-            mock.patch.object(mag_memory, "_find_binary", return_value=None),
-            mock.patch.object(
-                mag_memory,
-                "_binary_version",
-                return_value="9.8.7",
-                create=True,
-            ),
-            mock.patch(
-                "mag_memory._download.download_binary",
-                return_value="/tmp/mag",
-            ) as download_binary,
-            mock.patch.object(mag_memory.os, "execvp") as execvp,
-        ):
-            mag_memory.main()
-
-        download_binary.assert_called_once_with("9.8.7")
-        execvp.assert_called_once_with("/tmp/mag", ["/tmp/mag", *sys.argv[1:]])
+    def test_wrapper_has_no_independent_binary_version_constant(self):
+        self.assertFalse(hasattr(mag_memory, "_BINARY_VERSION"))
 
 
 if __name__ == "__main__":
